0685df56dbfeb09cffeaffaf873bdc55bbeda104
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-11-28 10:20:08 +0200

    rearranged folders and task 1 done

---
+++ javascript/16 paskaita - 11.28 - js ivadas/main.js
@@ -0,0 +1,15 @@
+// Task 1
+const letterA = "a";
+const letterB = "b";
+const letterC = "c";
+const letterD = "d";
+const letterE = "e";
+
+const letter1 = 1;
+const letter2 = 2;
+const letter3 = 3;
+const letter4 = 4;
+const letter5 = 5;
+
+const jsHasTooManyFrameworks = true;
+const jsHasRidiculousThingsInIt = true;
