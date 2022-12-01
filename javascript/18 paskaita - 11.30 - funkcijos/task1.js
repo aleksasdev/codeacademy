419e0fcaf588ce32a33a7864968c7dc7d4b495cf
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-01 08:37:49 +0200

    refactured every function of the task

--- javascript/18 paskaita - 11.30 - funkcijos/task1.js
+++ javascript/18 paskaita - 11.30 - funkcijos/task1.js
@@ -15,59 +15,67 @@ function doDivide(props){
 
 // 2) Sukurti funkciją, kuri į konsolę išvestų visus jai duotojo masyvo elementus nuo pirmo iki paskutinio.
 function getArrayElements(array){
+   let outputArray = []
    for(let arrayElement of array){
-      console.log(arrayElement)
+      outputArray.push(arrayElement);
    }
+   return outputArray;
 }
 
 // 3) Sukurti funkciją, kuri į konsolę išvestų visus jai duotojo masyvo elementus nuo paskutinio iki pirmojo.
 function getArrayElementsReversed(array){
+   let outputArray = []
    for(let arrayElement of array.reverse()){
-      console.log(arrayElement)
+      outputArray.push(arrayElement);
    }
+   return outputArray;
 }
 
 // 4) Sukurti funkciją, kuri surikiuotų gautąją informaciją (parametrą(masyvą(tik su string kintamaisiais))) abeceliškai ir ją išvestų į konsolę.
 function sortStrings(array){
-   console.log(array.sort());
+   return array.sort();
 }
 
 // 5) Sukurti funkciją, kuri surikiuotų gautąją informaciją (parametrą(masyvą(tik su number kintamaisiais))) nuo didžiausio iki mažiausio ir ją išvestų į konsolę.
 function sortIntegers(array){
-   console.log(array.sort((b, a) => a - b));
+   return array.sort((b, a) => a - b);
 }
 
 // 6) Sukurti funkciją, kuri į konsolę išvestų visus skaičius nuo pirmo duotojo parametro (imtinai) iki antrojo (imtinai). (paduodu 5,9 => grąžina 5,6,7,8,9)
 function getNumbersFromTo(num1, num2){
+   let outputArray = [];
    for(let i = num1; i < num2+1; i++){
-      console.log(i);
+      outputArray.push(i);
    }
+   return outputArray;
 }
 
 // 7) Sukurti funkciją, kuri į konsolę išvestų skaičius kas pirmąjį duotajį parametrą nuo antrojo duotojo parametro (imtinai) iki trečiojo duotojo parametro (imtinai). (paduodu 2,5,9 => grąžina 5,7,9)
 function getEveryNth(num1, num2, num3){
+   let outputArray = [];
    for(let i = num2; i < num3+1; i+=num1){
-      console.log(i);
+      outputArray.push(i);
    }
+   return outputArray;
 }
 
 // 8) Sukurkite funkciją, kuri paims du parametrus - vardą ir pavardę - ir grąžins sumą šių žodžių ilgių (t.y. "Petras", "Petrauskas" => 16).
 function getNameSurnameLength(name, surname){
-   console.log((name+surname).length);
+   return (name+surname.length);
 }
 
 // 9) Sukurkite funkciją, kuri pagal paduotą skaičių grąžins abecelės raidę (pvz. 0 => A; 1 => B; 2 => C ir t.t.). Hint: Jums reikės pirma sukurti Array su visomis raidėmis (["A", "B", C",..."Z"]), o funkcija paims pagal indeksą atitinkamą raidę.
 function getAlphabetFromNumber(number){
    let alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
-   console.log(alphabetArray[number-1]);
+   return alphabetArray[number-1];
 }
 
 // 10) Sukurkite funkciją, kuri paims tris parametrus (n1, n2, operator). Operator gali būti "sum", "sub", "div", "multi" (matematinės reikšmės - sudėti, atimti, padalinti, padauginti). n1 ir n2 bus skaičiai. Turite grąžinti atsakymą, kur n1 ir n2 atlikta matematinė operacija (pvz.: 5, 1, "sub" => 4; 6, 3, "sum" => 9...). Hint: Reikės funkcijoje naudoti if-else arba switch.
 function doNextLevelMathematics(n1, n2, operator){
-   if(operator==="+"||operator==="add"){console.log(n1+n2)}
-   if(operator==="-"||operator==="sub"){console.log(n1-n2)}
-   if(operator==="*"||operator==="multi"){console.log(n1*n2)}
-   if(operator==="/"||operator==="div"){console.log(n1/n2)}
+   if(operator==="+"||operator==="add"){return n1+n2}
+   if(operator==="-"||operator==="sub"){return n1-n2}
+   if(operator==="*"||operator==="multi"){return n1*n2}
+   if(operator==="/"||operator==="div"){return n1/n2}
 }
 
 // 11) Sukurkite dvi funkcijas: viena grąžina random skaičių nuo 1 iki 10 (įskaitant), kita - gautą skaičių pakelia kvadratu. Iškviesk abi funkcijas vienoje eilutėje (pvz. squareNum(generateRandomNumber()) => 1...100).
@@ -92,16 +100,18 @@ function whatIsThis(array, secondArray){
          newArray.push(array[i])
       }
    }
-   console.log(newArray);
+   return newArray;
 }
 
 // EXTRA 1 Sukurti funkciją, kuri atlieka Fizz Buzz testą nuo-iki duotųjų parametrų.
 function doFizzBuzzOnSteroids(from, to){
-   for(let i = from; i < to; i++){
-      if(i/3 % 2 === 0 && i/5 % 2 === 0){console.log(i,"FizzBuzz")}
-      else if(i/3 % 2 === 0){console.log(i,"Fizz")}
-      else if(i/5 % 2 === 0){console.log(i,"Buzz")}
-      else{console.log(i)};
+   let outputArray = [];
+   for(let i = from; i < to+1; i++){
+      if(i/3 % 2 === 0 && i/5 % 2 === 0){outputArray.push(i+" FizzBuzz")}
+      else if(i/3 % 2 === 0){outputArray.push(i+" Fizz")}
+      else if(i/5 % 2 === 0){outputArray.push(i+" Buzz")}
+      else{outputArray.push(i)};
    }
+   return outputArray;
 }
 
