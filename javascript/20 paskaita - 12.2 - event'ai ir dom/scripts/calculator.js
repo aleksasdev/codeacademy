81b5f20d7c6d77b17aebffeae42981340559dfcd
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-04 18:28:38 +0200

    calculator done

--- javascript/20 paskaita - 12.2 - event'ai ir dom/scripts/calculator.js
+++ javascript/20 paskaita - 12.2 - event'ai ir dom/scripts/calculator.js
@@ -22,13 +22,14 @@ const containerScreen = document.createElement('div');
 const currentOperation = document.createElement('p');
 const containerOperations = document.createElement('div');
 const containerCalculateOption = document.createElement('div');
+const containerAnswer = document.createElement('div');
 const buttonPlus = document.createElement('div');
 const buttonMinus = document.createElement('div');
 const buttonMultiply = document.createElement('div');
 const buttonDivide = document.createElement('div');
 //       Styling screen container
 containerScreen.style.width=valueCalculatorWidth;
-containerScreen.style.height="2rem";
+containerScreen.style.height="3rem";
 containerScreen.style.border=valueCalculatorBorderSize+" solid black";
 containerScreen.style.borderBottom="none";
 containerScreen.style.display="flex";
@@ -44,7 +45,7 @@ containerOperations.style.display="flex";
 containerOperations.style.justifyContent="space-around";
 containerOperations.style.alignItems="center";
 containerOperations.style.border=valueCalculatorBorderSize+" solid black";
-//       syling calculation choices
+//       syling calculation buttons
 let buttonsArray = [];
 buttonPlus.classList.add("calculate-option");buttonPlus.innerText="+";buttonsArray.push(buttonPlus);
 buttonMinus.classList.add("calculate-option");buttonMinus.innerText="-";buttonsArray.push(buttonMinus);
@@ -55,32 +56,114 @@ for(let button of buttonsArray){ // Add global styling for each button
    button.style.fontWeight="bold";
    button.style.cursor="pointer";
 }
+//       syling answer container
+containerAnswer.style.width=valueCalculatorWidth;
+containerAnswer.style.height="3rem";
+containerAnswer.style.border = valueCalculatorBorderSize+" solid black";
+containerAnswer.style.borderTop="none";
+containerAnswer.style.display="flex";
+containerAnswer.style.alignItems="center";
+containerAnswer.style.justifyContent="center";
+let buttonAnswer=document.createElement("p");
+buttonAnswer.innerText="Get\nAnswer";
+buttonAnswer.style.border="1px solid black";
+buttonAnswer.style.borderRadius="5px";
+buttonAnswer.style.padding="0.2rem";
+buttonAnswer.style.textAlign="center";
+buttonAnswer.style.cursor="pointer";
+containerAnswer.appendChild(buttonAnswer);
 //       Appending elements to calculator section
 sectionCalculator.appendChild(containerScreen);
 sectionCalculator.appendChild(containerOperations);
+sectionCalculator.appendChild(containerAnswer);
 containerOperations.append(buttonPlus,buttonMinus,buttonMultiply,buttonDivide)
 
 // Handle listeners
 //       Handle key strokes
 document.querySelector('html').addEventListener('keydown', (e)=>{
    if(!isNaN(parseInt(e.key))){ // Handle numbers
-      if(containerScreen.children.length>=35){return}; // Limit how many numbers there can fit
+      if(containerScreen.children.length>=30){return}; // Limit how many numbers there can fit
+      // Clear the screen if there's an answer on the screen
+      if(containerScreen.firstChild !== null&&containerScreen.firstChild.classList.contains("answer")){
+         while(containerScreen.firstChild){containerScreen.removeChild(containerScreen.firstChild)};
+      }
+      // Add clicked number to the screen container
       let newEntryToScreen = document.createElement('p');
       newEntryToScreen.innerText=e.key;
+      newEntryToScreen.style.fontWeight="bold";
+      newEntryToScreen.style.fontSize="1.5rem";
       containerScreen.appendChild(newEntryToScreen);
    }
    // Handle backspace
    if(e.key==="Backspace"&&containerScreen.children.length!==0){
       containerScreen.removeChild(containerScreen.lastChild);
-      containerScreen.children.length===0?containerScreen.removeChild(currentOperation):null;
+      try{
+         containerScreen.children.length===0?containerScreen.removeChild(currentOperation):null;
+      }catch{};
    }
 });
 //       Handle calculate buttons being clicked on
-let leftSideNumbers = []
 for(let button of buttonsArray){
    button.addEventListener('click',(e)=>{
       currentOperation.innerText=e.target.innerText;
+      currentOperation.style.fontWeight="bold";
+      currentOperation.style.fontSize="2rem";
       containerScreen.appendChild(currentOperation);
-      leftSideNumbers = containerScreen.children;
    });
 }
+//       Handle answer button being clicked
+buttonAnswer.addEventListener('click', (e)=>{
+   // Parse all children of the screen container into a string variable
+   // so we can split them into left side and right side
+   // and calculate properly
+   if(containerScreen.children.length===0){return false};
+   let arrayToString;
+   for(let element of containerScreen.children){arrayToString+=element.innerText};
+   arrayToString=arrayToString.replace('undefined','');
+   // Now loop through every value in the string array
+   // and find where the calculation choice resides in,
+   // then split that spot into left side numbers, right side numbers
+   // and operation variables
+   let leftSideNumbers;
+   let rightSideNumbers;
+   let operation;
+   let i=0;
+   for(let numberValue of arrayToString){
+      // Find the operation which is not a number
+      if(!parseInt(numberValue)){
+         operation = arrayToString[i]; // Assign operation to its variable first
+         // Then simply split the string into two
+         leftSideNumbers = arrayToString.split(operation)[0];
+         rightSideNumbers = arrayToString.split(operation)[1];
+         break;
+      }
+      i++;
+   }
+   // Now get the answer
+   let answer;
+   if(leftSideNumbers !== undefined && rightSideNumbers !== undefined
+      && operation !== undefined)
+   {
+      console.log(operation);
+      leftSideNumbers = parseInt(leftSideNumbers);
+      rightSideNumbers = parseInt(rightSideNumbers);
+      if(operation==="+"){answer=leftSideNumbers+rightSideNumbers};
+      if(operation==="-"){answer=leftSideNumbers-rightSideNumbers};
+      if(operation==="/"){answer=leftSideNumbers/rightSideNumbers};
+      if(operation==="*"){answer=leftSideNumbers*rightSideNumbers};
+   }
+   // Display the answer on the screen and console
+   if(answer!==undefined){
+      console.log(answer);
+      // Delete everything off the calculator screen
+      console.log(containerScreen.children);
+      while(containerScreen.firstChild){containerScreen.removeChild(containerScreen.firstChild)};
+      // Add the answer to the screen
+      let answerElement = document.createElement("p");
+      answerElement.classList+="answer";
+      answerElement.innerText="["+answer+"]";
+      answerElement.style.fontWeight="bold";
+      answerElement.style.fontSize="1.6rem";
+      containerScreen.appendChild(answerElement);
+   };
+});
