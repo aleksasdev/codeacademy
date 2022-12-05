0721efbbeda69827eded48b02403eeb7946a44e1
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-05 11:56:42 +0200

    made hide/show logic more universal

--- javascript/21 paskaita - 12.05 - dom praktika/scripts/main.js
+++ javascript/21 paskaita - 12.05 - dom praktika/scripts/main.js
@@ -1,13 +1,13 @@
 // Handle hide/show buttons being clicked on
 for(let hideButton of document.querySelectorAll('.hide-button')){
    hideButton.addEventListener('click', (e)=>{
-      let containerToHide = document.querySelector(hideButton.getAttribute('containerToHide'));
+      let elementsToShowOrHide = hideButton.parentElement.querySelectorAll('*>div');
       if(hideButton.id==="state-hide"){
-         containerToHide.style.display="none";
+         for(let element of elementsToShowOrHide){element.style.display="none"};
          hideButton.id="state-show";
          hideButton.innerText="Rodyti";
       }else{
-         containerToHide.style.display="unset";
+         for(let element of elementsToShowOrHide){element.style.display="unset"};
          hideButton.id="state-hide";
          hideButton.innerText="Slėpti";
       }
