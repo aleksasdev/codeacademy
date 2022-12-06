465a7d0678d69a698eb96cdde57cadec9c5916e6
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-06 15:34:55 +0200

    task 1 done

--- javascript/22 paskaita - 12.06 - metodai/scripts/main.js
+++ javascript/22 paskaita - 12.06 - metodai/scripts/main.js
@@ -1,4 +1,4 @@
-console.group("Math");
+console.groupCollapsed("Math");
 let word = "adasdsadasaf"
 let number = 50.426234324;
 console.log("toFixed()",number.toFixed(2));
@@ -6,7 +6,21 @@ console.log(word.slice(-1));
 console.groupEnd();
 
 // Task 1
-function isEnoughForMilk(){
-   let priceMilk = 3.19;
-   
-}
+// 1) Sukurk variable 'milkPrice'. 
+// Su JS patikrink ir informuok vartotoją console() 
+// ar jam reikės centų, ar nereikės (t.y. ar skaičius sveikas).
+console.group("Task1");
+let milkPrice = 3.1144;
+milkPrice%1!==0?console.log("You'll need cents, since the price is",milkPrice.toFixed(2))
+:console.log("You won't need any cents, the price is a whole number:",milkPrice);
+// ^
+// 2) Pakoreguok pirmą pratimą, kad console taip pat 
+// ir išmestų kainą - su dviem skaičias po kablelio (t.y. 3.00 Eur).
+
+// 3
+document.querySelector("#task1 form").addEventListener('submit',(e)=>{
+   e.preventDefault();
+   const price = parseFloat(document.querySelector('#task1 form .price').value).toFixed(2);
+   document.querySelector('#task1 .output').innerHTML=price;
+});
+console.groupEnd();
