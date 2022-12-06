dfc25eba0a5aaf525fc598d49febcb62873df43b
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-06 12:17:31 +0200

    added optimization to prime number function

--- javascript/21 paskaita - 12.05 - dom praktika/scripts/primeNumbersNoGoogle.js
+++ javascript/21 paskaita - 12.05 - dom praktika/scripts/primeNumbersNoGoogle.js
@@ -1,14 +1,46 @@
 function getPrimeNumbersWithoutGoogling(toNum){
-   var primeNumbers = [2];
+   let primeNumbers = [2];
+   let countOptimizationCycles=0;
    for(let i = 2; i <= toNum; i++){
       let isPrime=true;
       for(let alreadyDefinedPrimeNumber of primeNumbers){
+         // If we can divide a number we're testing by
+         // our already existing prime number, then the
+         // number we're testing IS NOT a prime number,
+         // because this means there's 3 factors now
+         // number/number, number/1, number/alreadyDefinedPrimeNumber.
+         // And a prime number can only have 2 factors MAX.
+         // let's say we're testing [10].
+         // 10/10 = 1 factor, 10/1 = 2 factor. which still makes it
+         // a prime number right? But if we start dividing [10]
+         // by our prime numbers, which starts from 2:
+         // 10/2 = 5, now we have 3 factors, so [10] IS
+         // NO LONGER a prime number and we can stop the loop
+         // and conclude that [10] isn't a prime number
          if(i % alreadyDefinedPrimeNumber === 0){
             isPrime=false;
+            break;
+         }
+         // Optimization code.
+         // If we're starting to test prime numbers that are
+         // more than half of the the number we're testing...
+         // Let's say, we're checking [100] and we're testing
+         // it with the prime number [53], we can break the loop instantly
+         // and not continue any further, since we'd still have to test 
+         // [59], [61], [67], etc..., but since these prime numbers
+         // are more than half of the number we're testing, then
+         // it's pointless, since it'll never divide into a whole number.
+         // 100/53 = 1.88679245283, 100/59 = 1.69491525424.
+         // So if the number hasn't been found as non-prime since this
+         // point, THEN we can safely assume that the number in question
+         // is prime and don't need to continue any further
+         if(i/2+2 < alreadyDefinedPrimeNumber && i > 100){
+            countOptimizationCycles++;
             break;
          }
       }
       isPrime?primeNumbers.push(i):null;
    }
+   console.log("Optimization ran this many times:",countOptimizationCycles);
    console.log(primeNumbers);
 }
---
+++ javascript/22 paskaita - 12.06 - metodai/scripts/main.js
@@ -0,0 +1,4 @@
+console.group("Math");
+let number = 50.426234324;
+console.log("toFixed()",number.toFixed(2));
+console.groupEnd();
