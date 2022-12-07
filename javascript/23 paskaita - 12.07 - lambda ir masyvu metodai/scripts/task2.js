0f7bab8b956c677f68770092d97fdca33eec8f4e
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-07 14:48:19 +0200

    task 2 partially done

---
+++ javascript/23 paskaita - 12.07 - lambda ir masyvu metodai/scripts/task2.js
@@ -0,0 +1,57 @@
+// Helper
+var taskIndex = 0;
+let getTaskNumber = ()=>{taskIndex++;return "["+taskIndex+"]";}
+
+console.group("Task2");
+
+// 1
+let doArrayToString = array=>{
+   let newString="";
+   array.forEach((element)=>newString+=element);
+   return newString;
+}
+console.log(getTaskNumber(),doArrayToString(["lmao","yoyo","aha"]));
+
+// 2
+let getReversedArray = array=>{
+   let reversedArray=[];
+   for(let i = array.length-1; i >= 0; i--){reversedArray.push(array[i])};
+   return reversedArray;
+}
+console.log(getTaskNumber(),getReversedArray(["One","Two","Three","Four","Five"]));
+
+// 3
+let getBiggestAndSmallestNumber = array=>{
+   // Get biggest number
+   for(let number of array){
+      // Take every number from given array and test it against
+      // all the other numbers in the same array, if it's smaller
+      // than at least one number, then it isn't the biggest
+      let isBiggestNumber=true;
+      for(let anotherNumber of array){
+         if(number<anotherNumber){isBiggestNumber=false;break};
+      }
+      if(isBiggestNumber){console.log("Biggest number is:",number);break};
+   };
+
+   // Get smallest number
+   for(let number of array){
+      // We do the same thing as before, but in reverse, if at least
+      // one number from the array is smaller than the number in
+      // question, we can conclude that the number in question isn't
+      // the smallest number
+      let isSmallestNumber=true;
+      for(let anotherNumber of array){
+         if(number>anotherNumber){isSmallestNumber=false;break};
+      }
+      if(isSmallestNumber){console.log("Smallest number is:",number);break};
+   };
+
+   // Using the min/max methods, YES, I knew they exist all along >:)
+   console.log("[min/max method] biggest number is:",array.max());
+   console.log("[min/max method] smallest number is:",array.min());
+}
+console.log(getTaskNumber());
+getBiggestAndSmallestNumber([4,5,11,44,667,743,222,1112,3,4342244,3242422,3,2.5]);
+
+console.groupEnd();
