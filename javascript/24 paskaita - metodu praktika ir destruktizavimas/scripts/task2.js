8578b7d3360b9413a0c2ab9eb72ef87ddbe22770
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-08 14:48:38 +0200

    some minor fixes and tests

--- javascript/24 paskaita - metodu praktika ir destruktizavimas/scripts/task2.js
+++ javascript/24 paskaita - metodu praktika ir destruktizavimas/scripts/task2.js
@@ -1,16 +1,16 @@
 // 1
 const foodArray=[
    {id:0,name:"burger",inStock:false,primeCost:0.3,cost:1.3},
-   {id:1,name:"fries",inStock:false,primeCost:0.2,cost:1.3},
+   {id:1,name:"fries",inStock:false,primeCost:0.24,cost:1.31},
    {id:2,name:"steak",inStock:true,primeCost:0.4,cost:2},
-   {id:3,name:"cola",inStock:true,primeCost:0.1,cost:1},
-   {id:4,name:"pepsi",inStock:true,primeCost:0.1,cost:1.1},
+   {id:3,name:"cola",inStock:true,primeCost:0.1,cost:13},
+   {id:4,name:"pepsi",inStock:true,primeCost:0.13,cost:1.12},
    {id:5,name:"dumplings",inStock:false,primeCost:0.2,cost:2},
    {id:5,name:"beetroot soup",inStock:true,primeCost:0.2,cost:1.4},
-   {id:6,name:"pizza",inStock:true,primeCost:0.5,cost:2.6},
-   {id:7,name:"ice cream",inStock:true,primeCost:0.1,cost:1.4},
+   {id:6,name:"pizza",inStock:true,primeCost:0.51,cost:2.6},
+   {id:7,name:"ice cream",inStock:true,primeCost:0.12,cost:1.49},
    {id:8,name:"vegan soup",inStock:false,primeCost:0.5,cost:1.4},
-   {id:9,name:"pickle soup",inStock:false,primeCost:0.1,cost:1.3},
+   {id:9,name:"pickle soup",inStock:false,primeCost:0.13,cost:1.34},
 ]
 console.group("Task 2");
 
@@ -35,7 +35,7 @@ const foodArrayProfit = foodArrayInStock.map(element=>{
       profit:(element.cost-element.primeCost).toFixed(2)+"eur"
    }
 });
-console.log("Profit\n",foodArrayProfit);
+console.log("Profit from items\n",foodArrayProfit);
 
 // 4.2
 // God, why have I sinned so terribly? This very function's existence
@@ -71,7 +71,7 @@ console.log("Profit from orders\n",foodArrayProfitFromOrders);
 const foodArrayReport = foodArrayProfitFromOrders.map(element=>{
    return {
       ordered: element.name+":"+element.orderedAmount,
-      totalCost: element.cost*element.orderedAmount+"eur"
+      totalCost: (element.cost*element.orderedAmount).toFixed(2)+"eur"
    }
 });
 console.log("Full report\n",foodArrayReport);
