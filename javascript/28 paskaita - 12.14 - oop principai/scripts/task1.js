84bce366adff9073d86b662f4caceef46ee3ebbb
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-14 13:16:04 +0200

    task1 done with improved way of handling things

--- javascript/28 paskaita - 12.14 - oop principai/scripts/task1.js
+++ javascript/28 paskaita - 12.14 - oop principai/scripts/task1.js
@@ -1,30 +1,31 @@
 class LogName{
-   #name;
-   #surname;
-   #fullname;
+   #namesArray=[];
    #outputElement;
-   constructor(fullname,outputElement){
-      this.#fullname=fullname;
+   constructor(outputElement){
       this.#outputElement=outputElement;
-      this.#parseFullname();
    }
-   #parseFullname(){
-      let parsedName=this.#fullname.split(" ");
-      this.#name=parsedName[0][0].toUpperCase()+parsedName[0].toLowerCase().slice(1);
-      this.#surname=parsedName[1][0].toUpperCase()+parsedName[1].toLowerCase().slice(1);
+   add(fullname){
+      let splitFullname = fullname.split(" ");
+      let name = splitFullname[0][0].toUpperCase()+splitFullname[0].toLowerCase().slice(1);
+      let surname = splitFullname[1][0].toUpperCase()+splitFullname[1].toLowerCase().slice(1);
+      this.#namesArray.push({name:name,surname:surname});
    }
-   appendToOutput(){
-      this.#outputElement.innerHTML+=(
-         `
-         <tr>
-            <td>${this.#name}</td>
-            <td>${this.#surname}</td>
-         </tr>
-         `
-      )
+   flush(){
+      this.#outputElement.innerHTML="";
+      this.#namesArray.forEach(element=>{
+         this.#outputElement.innerHTML+=
+            `
+            <tr>
+               <td>${element.name}</td>
+               <td>${element.surname}</td>
+            </tr>
+            `
+      });
    }
 }
+const log = new LogName(document.querySelector("#task1 table tbody"));
 document.querySelector("#task1 form").addEventListener('submit',e=>{
    e.preventDefault();
-   new LogName(e.target.elements[0].value,document.querySelector("#task1 table tbody")).appendToOutput();
+   log.add(e.target.elements[0].value);
+   log.flush();
 });
