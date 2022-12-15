008d4b5d4d8245d879d4be40df0e70ff32316a87
Aleksas <aleksas.ivanauskas23@gmail.com>
2022-12-15 11:56:51 +0200

    small refactor

--- javascript/29 paskaita - 12.15 - komponentu kurimas/scripts/element.js
+++ javascript/29 paskaita - 12.15 - komponentu kurimas/scripts/element.js
@@ -4,8 +4,6 @@ export default class Element{
    #element;
    constructor(props){
       this.#props = props;
-      this.#outputContainer = this.#props.outputContainer;
-      this.#element = document.createElement(this.#props.type);
       this.#parse();
    }
    getElement(){return this.#element};
@@ -27,6 +25,8 @@ export default class Element{
       this.#element.appendChild(this.child);
    }
    #parse(){
+      this.#outputContainer = this.#props.outputContainer;
+      this.#element = document.createElement(this.#props.type);
       // Add text to the main element if available
       if(this.#props.text){this.#element.innerText=this.#props.text};
       // Add attributes to the main element we're creating
