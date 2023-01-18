37a76b5fe9c820400ee599f9aec8ef56d1045ef7
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-18 11:02:13 +0200

    footer done

--- react/51-paskaita.praktika/src/components/main/Footer.jsx
+++ react/51-paskaita.praktika/src/components/main/Footer.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export const Footer = () => {
    return (
       <footer>
-         
+         <div>©Dragon Ball Chat 2023</div>
+         <ul className="footer-buttons">
+            <div className="nav-button">Terms</div>
+            <div className="nav-button">Support</div>
+            <div className="nav-button">About</div>
+         </ul>
       </footer>
    )
 }
