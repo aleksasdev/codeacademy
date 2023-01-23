d60d1eee77c2d7170d0940837aad75cd879ecfa2
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-23 09:00:29 +0200

    added a way to delete from server

--- react/52-paskaita.restaurant/src/components/pages/Edit.jsx
+++ react/52-paskaita.restaurant/src/components/pages/Edit.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useContext } from 'react'
+import {  OrdersContext } from './../../App';
+import { Loading } from './../misc/Loading';
+import { Order } from './Order';
+import { nanoid } from 'nanoid';
 
 export const Edit = () => {
+
+   const ordersContext = useContext(OrdersContext);
+
    return (
-      <div>Edit</div>
+      <section id="edit">
+         {ordersContext.orders
+         ?
+            ordersContext.orders.map(entry => <Order key={nanoid()} data={entry} editable={true} />)            
+         :
+            <Loading />
+         }
+      </section>
    )
 }
--- react/52-paskaita.restaurant/src/components/pages/Order.jsx
+++ react/52-paskaita.restaurant/src/components/pages/Order.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { DATABASE_URL } from '../../App';
 import './pages.css';
+import { OrdersContext } from './../../App';
+import { useContext } from 'react';
 
 export const Order = (props) => {
+
+   const ordersContext = useContext(OrdersContext);
+
+   function handleDelete(){
+      const filteredArray = ordersContext.orders.filter(entry => entry.id !== props.data.id);
+      ordersContext.setOrders(filteredArray);
+
+      fetch(`${DATABASE_URL}/${props.data.id}`, {
+         method: "DELETE"
+      })
+   }
 
    return (
       <div id={props.data.id} className='order-card'>
@@ -9,6 +23,10 @@ export const Order = (props) => {
          <p>{props.data.people}</p>
          <h3>Price</h3>
          <p>{props.data.price}$</p>
+
+         {props.editable &&
+            <div className="button-style" onClick={handleDelete}>Delete</div>
+         }
       </div>
    )
 }
