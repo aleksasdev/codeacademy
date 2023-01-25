e5faaff529d4c465cfd09f41d80cf9f4e95257cd
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-25 12:54:53 +0200

    added all the necessary errors, only stylizing remains

--- react/53-paskaita.validation/src/App.js
+++ react/53-paskaita.validation/src/App.js
@@ -1,7 +1,7 @@
 
 import { ValidInput } from './components/validationForm/ValidInput';
 import './index.css';
-import { HAVE_EMAIL_SIGN, MINIMUM_LENGTH_8 } from './components/validationForm/requirements';
+import { HAVE_EMAIL_SIGN, MINIMUM_LENGTH_8, HAVE_GMAIL_PREFIX, MINIMUM_AGE_13 } from './components/validationForm/requirements';
 import { ValidationForm } from './components/validationForm/ValidationForm';
 
 function App() {
@@ -11,13 +11,13 @@ function App() {
             <p>Username</p>
             <ValidInput name="username" requirements={[MINIMUM_LENGTH_8]} />
             <p>Email</p>
-            <ValidInput name="email" requirements={[HAVE_EMAIL_SIGN]} />
+            <ValidInput name="email" requirements={[HAVE_EMAIL_SIGN, HAVE_GMAIL_PREFIX]} />
             <p>Password</p>
             <ValidInput name="password" requirements={[MINIMUM_LENGTH_8]} />
             <p>Repeat Password</p>
             <ValidInput name="repeatPassword" requirements={[MINIMUM_LENGTH_8]} />
             <p>Age</p>
-            <ValidInput name="age" requirements={[]} />
+            <ValidInput name="age" requirements={[MINIMUM_AGE_13]} />
          </ValidationForm>
       </>
    );
--- react/53-paskaita.validation/src/components/validationForm/requirements.js
+++ react/53-paskaita.validation/src/components/validationForm/requirements.js
@@ -1,4 +1,5 @@
 export const MINIMUM_LENGTH_8 = "min8";
+export const MINIMUM_AGE_13 = "age13";
 export const HAVE_EMAIL_SIGN = "@";
 export const HAVE_GMAIL_PREFIX = ".gmail";
 
@@ -23,9 +24,15 @@ export function doesMeetRequirement(requirement, currentValue){
    if(requirement === MINIMUM_LENGTH_8){
       if(currentValue.length < 8) return "Your input has to be 8 characters long";
    }
+
+   else if(requirement === MINIMUM_AGE_13){
+      if(parseInt(currentValue) < 13) return "You can't use this site if you aren't at least 13 years old";
+   }
+
    else if(requirement === HAVE_EMAIL_SIGN){
       if(!currentValue.includes("@")) return "You have to put '@' symbol";
    }
+
    else if(requirement === HAVE_GMAIL_PREFIX){
       if(!currentValue.includes("gmail.com")) return "You have to use a gmail account";
    }
