5613c8e602f94dd3393b4b961ac0d2ef3f0661bd
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-25 12:48:56 +0200

    added all the required input fields for the task

--- react/53-paskaita.validation/src/App.js
+++ react/53-paskaita.validation/src/App.js
@@ -11,6 +11,12 @@ function App() {
             <ValidInput name="username" requirements={[MINIMUM_LENGTH_8, HAVE_EMAIL_SIGN]} />
             <p>Email</p>
             <ValidInput name="email" requirements={[HAVE_EMAIL_SIGN]} />
+            <p>Password</p>
+            <ValidInput name="password" requirements={[HAVE_EMAIL_SIGN]} />
+            <p>Repeat Password</p>
+            <ValidInput name="repeatPassword" requirements={[HAVE_EMAIL_SIGN]} />
+            <p>Age</p>
+            <ValidInput name="age" requirements={[HAVE_EMAIL_SIGN]} />
          </ValidationForm>
       </>
    );
