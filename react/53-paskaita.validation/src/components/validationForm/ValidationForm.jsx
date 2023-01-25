e1e3b0b731f219429e36b2989189e8fc4eb05af4
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-25 13:14:35 +0200

    removed unnecessary .css file

--- react/53-paskaita.validation/src/components/validationForm/ValidationForm.jsx
+++ react/53-paskaita.validation/src/components/validationForm/ValidationForm.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { createValues } from './createValues';
 import { createInputs } from './createInputs';
-import './index.css';
 
 export const ValidationForm = (props) => {
 
